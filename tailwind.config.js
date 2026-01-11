c47f5f7bbf85826cec3fc62b79e1a5f949929039
Dev_Qu <109199769+shadiq14@users.noreply.github.com>
2026-01-11 18:14:43 +0700

    Create  tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,jsx}',
+    './src/components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ec4899', // pink-600
+      },
+    },
+  },
+  plugins: [],
+}
